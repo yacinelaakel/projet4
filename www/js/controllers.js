1407a4426d1e46e18918ba5ed1a44dec45a1287f
YLaakel <yacine.laakel@hotmail.fr>
2016-12-06 21:00:20 +0100

    Affichage de la date des cours à venir dans 'Dates' OK

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,6 +12,24 @@ angular.module('multilingua.controllers', [])
   //});
 })
 
-.controller('DatesCtrl', function($scope) {
+.controller('DatesCtrl', function($scope, $http) {
+  //récupérer les dates depuis firebase
+  var url = 'https://projet4-23e35.firebaseio.com/dates.json';
+  var currentDate = new Date();
+  // //Petit problème avec l'heure : elle a une heure de retard par rapport à moi
+  currentDate.setHours(currentDate.getHours()+1);
+  
+  var items = [];
+  $http.get(url).success(function(data) {
+    angular.forEach(data, function(value, key) {
+      //Format "YYYY-MM-DDTHour"
+      var uneDate = new Date(value.date + "T" + value.heure);
+      //On affiche que les dates futures
+      if(uneDate > currentDate) {
+        items.push({date: value.date, heure: value.heure});
+      }
+    });
+  });
+  $scope.items = items;
 
 });
