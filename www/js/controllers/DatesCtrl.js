a1f935b95eeb3fe43cb9d6047fc753d48724c642
YLaakel <yacine.laakel@hotmail.fr>
2016-12-25 13:08:08 +0100

    Correction de bug push notif

--- www/js/controllers/DatesCtrl.js
+++ www/js/controllers/DatesCtrl.js
@@ -1,8 +1,21 @@
-multilingua.controller('DatesCtrl', function($scope, $stateParams, Dates) {
+multilingua.controller('DatesCtrl', function($scope, $stateParams, Dates, $cordovaLocalNotification) {
 
   //Toutes les dates des cours à venir
   Dates.get(function(items) {
     $scope.items = items;
+    var id = 0;
+    angular.forEach($scope.items, function(value, key) {
+        //Format "YYYY-MM-DDTHour"
+        var uneDate = new Date(value.date + "T" + value.heure);
+        var alarmTime = uneDate;
+      	alarmTime.setHours(alarmTime.getHours() -1);
+      	$cordovaLocalNotification.schedule({
+        	id: id++,
+        	date: alarmTime,
+        	message: "Un cours a lieu dans une heure",
+        	title: "Multilingua"
+      	}).then(function() {});
+    });
   });
 
 });
--- www/js/services/Dates.js
+++ www/js/services/Dates.js
@@ -1,4 +1,4 @@
-multilingua.factory("Dates", ["$firebaseObject", function($firebaseObject, $cordovaLocalNotification) {
+multilingua.factory("Dates", ["$firebaseObject", function($firebaseObject) {
     var ref = firebase.database().ref();
     var dates = $firebaseObject(ref.child('dates'));
 
@@ -7,24 +7,13 @@ multilingua.factory("Dates", ["$firebaseObject", function($firebaseObject, $cord
   		get: function(cb) {
         var currentDate = new Date();
         var items = [];
-        var id = 0;
         dates.$loaded().then(function() {
           angular.forEach(dates, function(value, key) {
-            console.log(value.date + " " + value.heure);
             //Format "YYYY-MM-DDTHour"
             var uneDate = new Date(value.date + "T" + value.heure);
             //On affiche que les dates futures
             if(uneDate > currentDate) {
               //Associe un push une heure avant chaque date future
-              var alarmTime = uneDate;
-              alarmTime.setHours(alarmTime.getHours() -1);
-              $cordovaLocalNotification.schedule({
-                id: id++,
-                date: alarmTime,
-                message: "Un cours a lieu dans une heure",
-                title: "Multilingua"
-              }).then(function() {});
-
               items.push({date: value.date, heure: value.heure});
             }
           });
